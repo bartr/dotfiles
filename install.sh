57c28ad9c9609bea41500e675a992e91434c8f47
bartr <bartr@microsoft.com>
2021-03-15 18:40:55 -0500

    updated

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-date >> /home/codespace/status
-echo "(dotfiles) starting ..." >> /home/codespace/status
+date >> ~/status
+echo "(dotfiles) starting ..." >> ~/status
 
 # source the bashrc_patch from dotfiles
-echo "" >> /home/codespace/.bashrc
-echo "source /workspaces/.codespaces/.persistedshare/dotfiles/.bashrc" >> /home/codespace/.bashrc
+echo "" >> ~/status
+echo "source ~/dotfiles/my_bashrc" >> ~/.bashrc
 
-mkdir -p /home/codespace/.local/bin
+mkdir -p ~/.local/bin
 
 sudo mkdir -p /etc/bash_completion.d
-sudo cp /workspaces/.codespaces/.persistedshare/dotfiles/kubectl /etc/bash_completion.d
+sudo cp ~/dotfiles/kubectl /etc/bash_completion.d
 
 git config --global user.name bartr
 git config --global user.email bartr@microsoft.com
@@ -19,12 +19,12 @@ git config --global pull.rebase false
 git config --global init.defaultbranch main
 git config --global core.pager more
 
-echo "(dotfiles) Installing packages ..." >> /home/codespace/status
+echo "(dotfiles) Installing packages ..." >> ~/status
 # install / update key apps
 DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends apt-utils dialog curl git jq httpie bash-completion
 DEBIAN_FRONTEND=dialog
 
-echo "(dotfiles) Done" >> /home/codespace/status
-date >> /home/codespace/status
+echo "(dotfiles) Done" >> ~/status
+date >> ~/status
