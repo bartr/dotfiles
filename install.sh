a32b5329b5e01b7ab2ddac00d490097bdec27055
bartr <bartr@microsoft.com>
2021-03-16 05:05:10 +0000

    testing

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ date >> /home/codespace/status
 echo "(dotfiles) starting ..." >> /home/codespace/status
 
 # source the bashrc_patch from dotfiles
-#echo "" >> /home/codespace/.bashrc
-#echo "source /home/codespace/dotfiles/my_bashrc" >> /home/codespace/.bashrc
+echo "" >> /home/codespace/.bashrc
+echo "source /home/codespace/dotfiles/my_bashrc" >> /home/codespace/.bashrc
 
-#mkdir -p /home/codespace/.local/bin
+mkdir -p /home/codespace/.local/bin
 
-#sudo mkdir -p /etc/bash_completion.d
+sudo mkdir -p /etc/bash_completion.d
 #sudo cp /home/codespace/dotfiles/kubectl /etc/bash_completion.d
 
 #git config --global user.name bartr
@@ -22,11 +22,11 @@ echo "(dotfiles) starting ..." >> /home/codespace/status
 #echo "(dotfiles) Installing packages ..." >> /home/codespace/status
 
 # install / update key apps
-#DEBIAN_FRONTEND=noninteractive
-#sudo apt-get update
-#sudo apt-get install -y --no-install-recommends apt-utils dialog curl git jq httpie bash-completion
-#DEBIAN_FRONTEND=dialog
+DEBIAN_FRONTEND=noninteractive
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends apt-utils dialog curl git jq httpie bash-completion
+DEBIAN_FRONTEND=dialog
 
-#echo "(dotfiles) Done" >> /home/codespace/status
-#date >> /home/codespace/status
+echo "(dotfiles) Done" >> /home/codespace/status
+date >> /home/codespace/status
 pwd >> /home/codespace/status
