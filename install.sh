77eb5f2fef7930f4a5be773ce5c35d709db6da1a
bartr <bartr@microsoft.com>
2021-03-16 00:03:06 +0000

    testing

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 date >> /home/codespace/status
 echo "(dotfiles) starting ..." >> /home/codespace/status
 
-echo pwd >> /home/codespace/status
+pwd >> /home/codespace/status
 
 echo "(dotfiles) Done" >> /home/codespace/status
 date >> /home/codespace/status
