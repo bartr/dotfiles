b35418f7db50af142eefb75e6c34ef61eb76dfe3
bartr <bartr@microsoft.com>
2021-03-19 18:47:14 +0000

    updated

--- install.sh
+++ install.sh
@@ -22,6 +22,7 @@ git config --global core.whitespace blank-at-eol,blank-at-eof,space-before-tab
 git config --global pull.rebase false
 git config --global init.defaultbranch main
 git config --global core.pager more
+sudo git config --system credential.helper '!f() { sleep 1; echo "username=${GIT_COMMITTER_NAME}"; echo "password=${GITHUB_TOKEN}"; }; f'
 
 echo "(dotfiles) Installing packages ..." >> ~/status
 
