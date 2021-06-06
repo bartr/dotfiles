c99d7288019ea61125a44420a99363b6429095da
bartr <bartr@microsoft.com>
2021-06-05 19:56:29 -0500

    updated install

--- install.sh
+++ install.sh
@@ -17,6 +17,10 @@ git config --global core.whitespace blank-at-eol,blank-at-eof,space-before-tab
 git config --global pull.rebase false
 git config --global init.defaultbranch main
 git config --global core.pager more
+git config --global fetch.prune true
+
+# delete merged local branches
+git config --global alias.gone "! git checkout main && git fetch -pa && git pull &&  git for-each-ref --format '%(refname:short) %(upstream:track)' | awk '\$2 == \"[gone]\" {print \$1}' | xargs -r git branch -D"
 
 # Codespaces sets the env vars for you
 # I override these values in my_bashrc with a "full PAT"
@@ -28,6 +32,7 @@ sudo git config --system credential.helper '!f() { sleep 1; echo "username=${GIT
 git config --global url.https://github.com/bartr/.insteadOf b://
 git config --global url.https://github.com/cloudatx/.insteadOf c://
 git config --global url.https://github.com/.insteadOf g://
+git config --global url.https://github.com/lnkdus/.insteadOf l://
 git config --global url.https://github.com/retaildevcrews/.insteadOf r://
 
 # install / update key utils
